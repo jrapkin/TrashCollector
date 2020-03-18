865a305d66deaec2b1cd865efc6b6cfd4c34eb6b
jrapkin <jrap1243@gmail.com>
2020-03-18 09:20:09 -0500

    init

---
+++ Trash_Collector_Application/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Trash_Collector_Application.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
