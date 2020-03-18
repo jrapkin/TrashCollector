9e5fa86ab10cbfeb582060b701f467cd1f25a5f8
jrapkin <jrap1243@gmail.com>
2020-03-18 12:08:52 -0500

    Added Identity scaffolding for Login, Logout, and Register. Updated the OnGetAsync method and OnPostAsync methods to accomodate the included global member variable RoleManager.

---
+++ Trash_Collector_Application/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trash_Collector_Application.Models
+{
+	public class Customer
+	{
+		public int CustomerId { get; set; }
+		public string Name { get; set; }
+		[ForeignKey("")]
+		public string IdtentityUserId { get; set; }
+		public IdentityUser IdentityUser { get; set; }
+
+	}
+}
