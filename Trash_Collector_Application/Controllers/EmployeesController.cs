8e5a7fb1e9d2f4e28d7bad04b71ed15bd64bf283
jrapkin <jrap1243@gmail.com>
2020-03-19 09:45:08 -0500

    Implemented globalrouting into the startup configuration, and added authorize attribute to EmployeeController.

--- Trash_Collector_Application/Controllers/EmployeesController.cs
+++ Trash_Collector_Application/Controllers/EmployeesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using Trash_Collector_Application.Models;
 
 namespace Trash_Collector_Application.Controllers
 {
+    [Authorize(Roles = "Customer")]
     public class EmployeesController : Controller
     {
         private readonly ApplicationDbContext _context;
--- Trash_Collector_Application/Startup.cs
+++ Trash_Collector_Application/Startup.cs
@@ -12,6 +12,9 @@ using Trash_Collector_Application.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using Trash_Collector_Application.ActionFilters;
 
 namespace Trash_Collector_Application
 {
@@ -34,6 +37,12 @@ namespace Trash_Collector_Application
 				.AddEntityFrameworkStores<ApplicationDbContext>()
 				.AddDefaultUI()
 				.AddDefaultTokenProviders();
+			services.AddScoped<ClaimsPrincipal>(s =>
+				s.GetService<IHttpContextAccessor>().HttpContext.User);
+			services.AddControllers(config =>
+			{
+				config.Filters.Add(typeof(GlobalRouting));
+			});
 			services.AddControllersWithViews();
 			services.AddRazorPages();
 		}
